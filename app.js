05ab5e5320b5a761451930fda0291cbd323ebcee
Jarod Timms <jarod.timms@snhu.edu>
2022-08-13 14:19:31 -0500

    Added backend and front end security protections for th eapplication.

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const hbs = require('hbs');
+const passport = require('passport');
+
 require('./app_api/database/db');
+require('./app_api/config/passport');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
@@ -26,11 +31,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 // allow CORS
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE");
   next();
 });
@@ -39,6 +45,15 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/api', apiRouter);
+
+// Authorization error handler
+app.use(function(err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res
+      .status(401)
+      .json({ 'message': err.name + ': ' + err.message });
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,5 +70,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
